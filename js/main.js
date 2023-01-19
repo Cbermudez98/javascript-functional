523e64c1cb93be1c73d0b972df3c4236de690a6b
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2023-01-18 23:23:30 -0500

    Word on delete function

--- js/main.js
+++ js/main.js
@@ -1,56 +1,70 @@
 const list = [];
 
-const compose = (...functions) => data =>
-  functions.reduceRight((value, func) => func(value), data)
+const compose =
+  (...functions) =>
+  (data) =>
+    functions.reduceRight((value, func) => func(value), data);
 
-const attrToString = (obj = {}) => {
-  const keys = Object.keys(obj);
-  const attrs = [];
-  for(const key of keys) {
-    attrs.push(`${key}="${obj[key]}"`) 
-  }
-  return attrs.join("");
-}
+const attrToString = (obj = {}) => 
+  Object.keys(obj)
+  .map(key => `${key}="${obj[key]}"`)
+  .join("");
 
-const tagAttr = (obj) => (content = "") => `<${obj.tag} ${obj.attrs? "" : ""}${attrToString(obj.attrs)}>${content}</${obj.tag}>`;
+const tagAttr = (obj) => (content = "") =>
+    `<${obj.tag} ${obj.attrs ? "" : ""}${attrToString(obj.attrs)}>${content}</${
+      obj.tag
+    }>`;
 
-const tag = t => {
-  if (typeof t === "string") {
-    return tagAttr({tag: t});
-  } else {
-    return tagAttr(t);
-  }
-}
+const tag = (t) => typeof t === "string" ? tagAttr({ tag: t }) : tagAttr(t);
 
+const trashIcon = tag({ tag: "i", attrs: { class: "fas fa-trash-alt" } })("");
 
 const tableCell = tag("td");
-const tableCells = items => items.map(tableCell).join("");
+const tableCells = (items) => items.map(tableCell).join("");
 
 const tableRowTag = tag("tr");
-const tableRows = items => compose(tableRowTag, tableCells)(items);
+const tableRows = (items) => compose(tableRowTag, tableCells)(items);
 
-let description = $('#description');
-let calories = $('#calories');
-let carbs = $('#carbs');
-let protein = $('#protein');
+let description = $("#description");
+let calories = $("#calories");
+let carbs = $("#carbs");
+let protein = $("#protein");
 
-description.keypress(() => (description.removeClass("is-invalid")));
-calories.keypress(() => (calories.removeClass("is-invalid")));
-carbs.keypress(() => (carbs.removeClass("is-invalid")));
-protein.keypress(() => (protein.removeClass("is-invalid")));
+description.keypress(() => description.removeClass("is-invalid"));
+calories.keypress(() => calories.removeClass("is-invalid"));
+carbs.keypress(() => carbs.removeClass("is-invalid"));
+protein.keypress(() => protein.removeClass("is-invalid"));
+
+const removeItem = (index) => {
+  list.splice(index, 1);
+  updateTotal();
+  renderItems();
+  updateTotal()
+}
 
 const renderItems = () => {
   $("tbody").empty();
 
-  list.map(item => {
-    console.log(tableRows([item.description, item.calories, item.carbs, item.protein]));
-    $("tbody").append(tableRows([item.description, item.calories, item.carbs, item.protein]));
+  list.map((item, index) => {
+    const removeButton = tag({
+      tag: "button",
+      attrs: {
+        class: "btn btn-outline-danger",
+        onclick: `removeItem(${index})`,
+      },
+    })(trashIcon);
+
+    $("tbody").append(
+      tableRows([item.description, item.calories, item.carbs, item.protein, removeButton])
+    );
   });
 };
 
 const updateTotal = () => {
-  let calories = 0, carbs = 0, protein = 0;
-  list.map(item => {
+  let calories = 0,
+    carbs = 0,
+    protein = 0;
+  list.map((item) => {
     calories += item.calories;
     carbs += item.carbs;
     protein += item.protein;
@@ -58,15 +72,15 @@ const updateTotal = () => {
   $("#totalCalories").text(calories);
   $("#totalCarbs").text(carbs);
   $("#totalProtein").text(protein);
-}
+};
 
 const add = () => {
   const newItem = {
     description: description.val(),
     calories: Number(calories.val()),
     carbs: Number(carbs.val()),
-    protein: Number(protein.val())
-  }
+    protein: Number(protein.val()),
+  };
 
   list.push(newItem);
   cleanInputs();
@@ -81,13 +95,13 @@ const validateInputs = () => {
   protein.val() ? "" : protein.addClass("is-invalid");
 
   if (description.val() && calories.val() && carbs.val() && protein.val()) {
-    add()
+    add();
   }
-}
+};
 
 const cleanInputs = () => {
   description.val("");
   calories.val("");
   carbs.val("");
-  protein.val("")
-}
+  protein.val("");
+};
